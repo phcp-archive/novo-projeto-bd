71e39dda0911aa6447d33292eff0fc6b0810e8bc
Paulo Henrique <phcp@cin.ufpe.br>
2014-02-25 00:09:25 -0300

    Correções no front

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -10,7 +10,6 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     
     if @user_session.save
-      flash[:notice] = "Bem-vindo ao sistema!"
       redirect_back_or_default account_url(@current_user)
     else
       render :action => :new
@@ -19,7 +18,6 @@ class UserSessionsController < ApplicationController
 
   def destroy
     current_user_session.destroy
-    flash[:notice] = "Voce saiu do sistema."
     redirect_back_or_default new_user_session_url
   end
 end
